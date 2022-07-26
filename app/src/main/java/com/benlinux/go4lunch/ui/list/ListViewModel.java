e410159a08c34f76d3e03e251cc78d1956da76f9
Ben_Linux <benoit.vincent2707@gmail.com>
2022-07-26 17:57:02 +0200

    Set Facebook Login & background with logo

---
+++ app/src/main/java/com/benlinux/go4lunch/ui/list/ListViewModel.java
@@ -0,0 +1,19 @@
+package com.benlinux.go4lunch.ui.list;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ListViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ListViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is List View fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/benlinux/go4lunch/ui/map/MapViewModel.java
@@ -0,0 +1,19 @@
+package com.benlinux.go4lunch.ui.map;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public MapViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Map View fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
