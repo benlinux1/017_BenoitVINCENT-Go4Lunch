cee517685aed0c648a1767fe5e640599ca9445e8
Ben_Linux <benoit.vincent2707@gmail.com>
2022-09-02 12:01:31 +0200

    Edit : Init restaurant details layout & activity

--- app/src/main/java/com/benlinux/go4lunch/activities/RestaurantDetailsActivity.java
+++ app/src/main/java/com/benlinux/go4lunch/activities/RestaurantDetailsActivity.java
@@ -1,4 +1,31 @@
 package com.benlinux.go4lunch.activities;
 
-public class RestaurantDetailsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.benlinux.go4lunch.databinding.ActivityRestaurantDetailsBinding;
+
+public class RestaurantDetailsActivity extends AppCompatActivity {
+
+    private ActivityRestaurantDetailsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRestaurantDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
+        getRestaurantIdFromIntent();
+
+    }
+
+    private String getRestaurantIdFromIntent() {
+        Intent intent = getIntent();
+        String restaurantID = intent.getStringExtra("PLACE_ID");
+        return restaurantID;
+    }
 }
